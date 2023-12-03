db76adcea7ccdbbde5b5c734af95aae3de8578d0
Javier Ballester <Javirb26@gmail.com>
2023-12-03 14:33:28 -0500

    got basic information of products to output

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,37 @@
 window.onload = () => {
-    // viewAllCat();
-    searchCat(); // Remove the parentheses here
+    searchCat();
 }
-    const categorySelectEl = document.getElementById('selectCategoryEl');
-    const selectSpecificEl = document.getElementById('selectSpecific');
-    const outputEl = document.getElementById('output')
+
+const categorySelectEl = document.getElementById('selectCategoryEl');
+const selectSpecificEl = document.getElementById('selectSpecific');
+const outputEl = document.getElementById('output')
 
 let searchCat = () => {
-    
     categorySelectEl.onchange = () => {
-        if (categorySelectEl.selectedIndex === 0) {
-            selectSpecificEl.innerHTML = '';
-            selectSpecificEl.classList.add('d-none');
-        } else if (categorySelectEl.selectedIndex === 1) {
-            selectSpecificEl.classList.remove('d-none');
-            selectSpecificEl.innerHTML = '';
+        // Resetting previous selections and outputs
+        selectSpecificEl.innerHTML = '';
+        outputEl.innerHTML = '';
+        selectSpecificEl.classList.toggle('d-none', categorySelectEl.selectedIndex === 0);
+
+        if (categorySelectEl.selectedIndex === 1) {
+
+            const defaultOption = document.createElement('option');
+            defaultOption.textContent = "Please Choose a Category";
+            defaultOption.value = "";
+            selectSpecificEl.appendChild(defaultOption);
+
 
             fetch('http://localhost:8081/api/categories')
                 .then((res) => res.json())
                 .then((categories) => {
-                    for (let category of categories) {
-                        // Create an <option> element and append it to the select
+                    categories.forEach(category => {
                         const optionEl = document.createElement('option');
-                        optionEl.value = category.id;
-                        optionEl.textContent = `${category.name}`;
+                        optionEl.value = category.categoryId;
+                        optionEl.textContent = category.name;
                         selectSpecificEl.appendChild(optionEl);
-                    }
+                    });
                 });
-        } else if (categorySelectEl.selectedIndex === 2){
+        } else if (categorySelectEl.selectedIndex === 2) {
 
             selectSpecificEl.innerHTML = '';
             selectSpecificEl.classList.add('d-none');
@@ -38,21 +42,36 @@ let searchCat = () => {
                     for (let product of products) {
                         // Create an <option> element and append it to the select
                         const listEl = document.createElement('li');
+                        const detailLink = document.createElement('a');
+                        detailLink.href = `../details.html?id=${product.productId}`;
+                        detailLink.innerHTML = 'See Details';
                         listEl.value = product.productId;
-                        listEl.textContent = `${product.productName}: $${product.unitPrice}, IN STOCK: ${product.unitsInStock}`;
+                        listEl.textContent = `${product.productName}: $${product.unitPrice}`;
+                        listEl.appendChild(detailLink);
                         outputEl.appendChild(listEl);
                     }
                 });
         }
-        // if(categorySelectEl.selectedIndex === 2) {}
+    }
+
+
+    selectSpecificEl.onchange = () => {
+        let selectedCategoryId = selectSpecificEl.value;
+        outputEl.innerHTML = '';
+
+        fetch(`http://localhost:8081/api/products`)
+            .then((res) => res.json())
+            .then((products) => {
+                let productsToShow = products.filter(product => product.categoryId.toString() === selectedCategoryId);
+                productsToShow.forEach(product => {
+                    const listEl = document.createElement('li');
+                    const detailLink = document.createElement('a');
+                    detailLink.href = `../details.html?productId=${product.productId}`;
+                    detailLink.innerHTML = 'See Details';
+                    listEl.textContent = `${product.productName}: $${product.unitPrice}`;
+                    listEl.appendChild(detailLink);
+                    outputEl.appendChild(listEl);
+                });
+            });
     }
 }
-
-
-
-
-
-
-
-
-
